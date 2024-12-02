18fce0a770388abe84fcb34015473134f6ca0dd7
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2024-12-02 20:34:57 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+class Button extends React.Component {
+    render() {
+        return(
+            <button> Button </button>
+        )
+    }
+}
+
+export default Button;
